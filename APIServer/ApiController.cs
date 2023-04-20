552b84fa7fc8b3df76678b8300aceedb661d804b
Sam Denton <samw.denton@gmail.com>
2023-04-20 17:18:54 +0100

    Looking to move API keys to a proxy.

---
+++ APIServer/ApiController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GitHubController : ControllerBase
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _githubApiKey;
+
+    public GitHubController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient();
+        _githubApiKey = Environment.GetEnvironmentVariable("GITHUB_API_KEY");
+    }
+
+    [HttpGet("repos/{user}/{repo}")]
+    public async Task<IActionResult> GetRepository(string user, string repo)
+    {
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", _githubApiKey);
+
+        var response = await _httpClient.GetAsync($"https://api.github.com/repos/{user}/{repo}");
+        response.EnsureSuccessStatusCode();
+
+        var repository = await response.Content.ReadAsStringAsync();
+        return Ok(repository);
+    }
+}
