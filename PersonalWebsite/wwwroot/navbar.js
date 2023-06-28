6d42000178b9039a202dafa1445b46965bc95cfe
Sam Denton <samw.denton@gmail.com>
2023-06-28 18:53:49 +0100

    Lots of layout tweeks.  Fixed the nav bar on mobile.  Removed duplicated nav bar.  Added more mouse controls to nav bar.  Removed some unused code.  Fixed search bar on mobile.  Fixed issue moving from any component to main.

--- PersonalWebsite/wwwroot/navbar.js
+++ PersonalWebsite/wwwroot/navbar.js
@@ -74,13 +74,13 @@ function fadeOut() {
     }
 }
 
-let navContainer, scrollContainer, nav, popupTip, tooltipTimeout;
-let isMouseOverNav = false;
+let navContainer, scrollContainer, nav, popupTip;
+let isMouseOverNav, isTouchActive, animationStarted = false;
 let currentTop = 0, prevTop = 0, startY = 0, startX = 0;
-let maxTop, containerHeight, navHeight, navItemsHeight, initialY;
-let animationStarted = false;
+let maxTop, containerHeight, navHeight, navItemsHeight, initialY, tooltipTimeout;
 const momentumFactor = 0.95;
 const fadeEffectStrength = 0.05;
+let menuCount = 1;
 
 function initScrollingMenu(navContainerSelector, scrollContainerSelector, navSelector) {
     navContainer = document.querySelector(navContainerSelector);
@@ -93,29 +93,29 @@ function initScrollingMenu(navContainerSelector, scrollContainerSelector, navSel
     navContainer.addEventListener("mousemove", onMouseMove);
     navContainer.addEventListener("mouseleave", onMouseLeave);
     navContainer.addEventListener("touchstart", onTouchStart, { passive: true });
-    navContainer.addEventListener("touchmove", onTouchMove, { passive: false });
     navContainer.addEventListener("touchend", onTouchEnd, { passive: true });
     setTimeout(() => {
         const navItemsContainer = nav.querySelector('.nav-items-container');
         const navItemsContainerClone1 = navItemsContainer.cloneNode(true);
         const navItemsContainerClone2 = navItemsContainer.cloneNode(true);
 
-        nav.prepend(navItemsContainerClone1);
-        nav.append(navItemsContainerClone2);
+        if (menuCount == 3) {
+            nav.prepend(navItemsContainerClone1);
+            nav.append(navItemsContainerClone2);
+        }
         containerHeight = navContainer.offsetHeight;
         navHeight = nav.offsetHeight;
         navItemsHeight = navItemsContainer.offsetHeight;
         maxTop = containerHeight - ((navHeight * 3) / 2);
 
-        currentTop = (containerHeight - navItemsHeight * 3) / 2; // Update currentTop calculation
+        currentTop = ((containerHeight - navItemsHeight * menuCount) / 2) + 50; // Update currentTop calculation
         scrollContainer.style.top = `${currentTop}px`;
-        applyEffects();
     }, 500); // Set the delay time in milliseconds (e.g., 500ms)
 }
 
 function calculateScrollSpeed(mouseY, startY, maxSpeed) {
     const distanceToStart = Math.abs(mouseY - startY);
-    const speed = (distanceToStart / (containerHeight / 2)) / 2;
+    const speed = (distanceToStart / (containerHeight / 1)) / 1;
 
     if (speed > maxSpeed) {
         return maxSpeed;
@@ -126,11 +126,12 @@ function calculateScrollSpeed(mouseY, startY, maxSpeed) {
 
 function onMouseEnter(event) {
     const navContainerRect = navContainer.getBoundingClientRect();
-    startY = event.clientY - navContainerRect.top;
+    //startY = event.clientY - navContainerRect.top;
+    startY = containerHeight / 2;
     startX = event.clientX - navContainerRect.left;
     // Show the tooltip
     popupTip.style.display = 'block';
-    popupTip.style.top = `${startY + 10}px`;
+    popupTip.style.top = `${startY - 45}px`;
     popupTip.style.left = `${startX - 10}px`;
     // Hide the tooltip after 3 seconds
     clearTimeout(tooltipTimeout);
@@ -146,7 +147,7 @@ function onMouseMove(event) {
     let moveX = event.clientX - navContainerRect.left;
 
     // Update the tooltip position
-    popupTip.style.top = `${moveY + 10}px`;
+    popupTip.style.top = `${moveY - 45}px`;
     popupTip.style.left = `${moveX - 10}px`;
     isMouseOverNav = true;
     navHeight = nav.offsetHeight;
@@ -154,10 +155,9 @@ function onMouseMove(event) {
     if (navHeight > containerHeight) {
         const mouseY = event.clientY - navContainerRect.top;
         const scrollPosition = (mouseY - startY) / (containerHeight / 2);
-        maxTop = containerHeight - ((navHeight * 3) / 2);
 
         // Calculate scrolling speed based on the mouse position
-        const maxSpeed = 0.1; // Set the maximum scrolling speed
+        const maxSpeed = 0.2; // Set the maximum scrolling speed
         const speed = calculateScrollSpeed(mouseY, startY, maxSpeed);
         const newTop = maxTop * scrollPosition * speed - (navHeight - containerHeight) / 2;
 
@@ -182,13 +182,37 @@ function animateMouseMove(event) {
     }
 }
 
+const bounceFactor = 0.5; // Change this value to control the strength of the bounce
+let hasBouncedTop = false;
+let hasBouncedBottom = false;
+
 function updatePosition(targetTop) {
     const smoothingFactor = 0.05; // Change this value to control the smoothness of the animation
     prevTop = currentTop; // Store the current top position before updating
-    currentTop += (targetTop - currentTop) * smoothingFactor;
+
+    // Calculate the new position
+    let newTop = currentTop + (targetTop - currentTop) * smoothingFactor;
+
+    // Adjust the target position if the new position is beyond the top boundary
+    if (newTop > 10 && !hasBouncedTop) {
+        targetTop = currentTop - Math.abs(newTop) * (bounceFactor + 0.2);
+        hasBouncedTop = true;
+    }
+
+    // Adjust the target position if the new position is beyond the bottom boundary
+    if (newTop < window.innerHeight - navHeight - 56 && !hasBouncedBottom) {
+        targetTop = currentTop - (newTop - (window.innerHeight - navHeight)) * bounceFactor;
+        hasBouncedBottom = true;
+    }
+
+    // Recalculate the new position with the adjusted target position
+    newTop = currentTop + (targetTop - currentTop) * smoothingFactor;
+
+    currentTop = newTop;
     scrollContainer.style.top = `${currentTop}px`;
-    applyEffects();
 }
+
+    //applyEffects();
 
 function applyMomentum(targetTop, momentum) {
     if (isTouchActive) { return };
@@ -225,53 +249,21 @@ function onMouseLeave(event) {
         // Reset the animationStarted flag when the momentum effect has ended
         animationStarted = false;
     }
+    hasBouncedTop = false;
+    hasBouncedBottom = false;
     popupTip.style.display = 'none';  // Hide the tooltip when the mouse leaves the navbar
 }
 
-let touchStartY = 0;
-let touchMoveThreshold = 10; // Set the threshold in pixels (e.g., 10 pixels)
-let isTouchActive = false;
-
 function onTouchStart(event) {
     isTouchActive = true;
-    touchStartY = event.touches[0].clientY;
-}
-
-function onTouchMove(event) {
-    const touch = event.touches[0];
-    const deltaY = touch.clientY - touchStartY;
-    touchStartY = touch.clientY;
-
-    // Only call preventDefault if the user has moved their finger beyond the threshold
-    if (Math.abs(deltaY) > touchMoveThreshold) {
-        event.preventDefault(); // Prevent scrolling in the background
-    }
-
-    const speedMultiplier = 10; // Adjust this value to control the scrolling speed
-    const newTop = currentTop + deltaY * speedMultiplier;
-    updatePosition(newTop);
+    navContainer.classList.remove("nav-container");
+    navContainer.classList.add("nav-container-touch");
 }
 
 function onTouchEnd(event) {
     isTouchActive = false;
-}
-
-
-function applyEffects() {
-    const navItems = nav.querySelectorAll('.item-fader');
-    const centerY = startY;
-
-    navItems.forEach((navItem) => {
-        const navItemRect = navItem.getBoundingClientRect();
-        const navContainerRect = navContainer.getBoundingClientRect();
-        const navItemCenterY = navItemRect.top + navItemRect.height / 2 - navContainerRect.top;
-        const distanceFromCenter = Math.abs(navItemCenterY - centerY);
-        const scaleFactor = 1 - (distanceFromCenter / containerHeight * fadeEffectStrength);
-        const opacityFactor = Math.abs(1 - (distanceFromCenter / (containerHeight / 2) * fadeEffectStrength));
-
-        navItem.style.transform = `scale(${scaleFactor})`;// translateY(-50%)
-        navItem.style.opacity = opacityFactor;
-    });
+    navContainer.classList.remove("nav-container-touch");
+    navContainer.classList.add("nav-container");
 }
 
 window.initScrollingMenu = initScrollingMenu;
