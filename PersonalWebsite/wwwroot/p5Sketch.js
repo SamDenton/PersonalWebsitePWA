13722d9f2fe4d8f548bade5ad24a3fd91d9f67c0
SamDentonHome <samw.denton@gmail.co.uk>
2023-08-22 00:34:32 +0100

    Managed to get the number of tensors consistent between generations, and implemented enongh optomisaitons to get 100 agents at a time running smoothly, at 1 6th speed :D

--- PersonalWebsite/wwwroot/p5Sketch.js
+++ PersonalWebsite/wwwroot/p5Sketch.js
@@ -18,19 +18,29 @@ let categoryAgent = 0x0001;
 let categoryGround = 0x0002;
 let MAX_ADJUSTMENT = 1;
 
+let genCount;
+let displayedFPS = 0;
+let displayedTimeLeft = 0;
+let frameCountSinceLastFPS;
+let lastFPSCalculationTime;
 let tickCount = 0;
 let simulationLength;
+let lastUIUpdateTime = 0;
+let UIUpdateInterval = 500;
+
+let topScoreEver = 0;
 
 let sketch = function (p) {
     let lastMuscleUpdate = 0;
     let muscleUpdateInterval = 1000; // Update every 100 ms
 
-    let fixedTimeStep = 1000 / simulationSpeed; // 60 updates per second for physics
+    let fixedTimeStep = 1000 / simulationSpeed; // 'simulationSpeed' updates per second for physics
     let accumulator = 0;
     let lastTime = 0;
-    let leadingAgent = getLeadingAgent();
+    let leadingAgent;
 
     p.setup = function () {
+        p.frameRate(60);
         p.createCanvas(canvasWidth, canvasHeight);
         setupMatterEngine(); // Initialize the Matter.js engine
         lastTime = p.millis();
@@ -56,6 +66,7 @@ let sketch = function (p) {
 
 
     function updatePhysics() {
+        leadingAgent = getLeadingAgent();
         if (leadingAgent) {
             if (p.millis() - lastMuscleUpdate > muscleUpdateInterval) {
                 for (let agent of agents) {
@@ -76,6 +87,7 @@ let sketch = function (p) {
     }
 
     function renderScene(p) {
+        leadingAgent = getLeadingAgent();
         if (leadingAgent) {
             offsetX = p.width / 4 - leadingAgent.centerBody.position.x;  // Center the leading agent on the canvas
 
@@ -114,11 +126,22 @@ let sketch = function (p) {
                     agent.render(p, offsetX);
                 }
             }
+            calculateFPS(p);
+            let currentTime = p.millis();
+            if (currentTime - lastUIUpdateTime > UIUpdateInterval) {
+                // Update the FPS, Gen No, and Time Left values
+                displayedTimeLeft = (simulationLength - tickCount) / (displayedFPS / simulationSpeed);
 
-            // Display frame rate
+                // Reset the last update time
+                lastUIUpdateTime = currentTime;
+            }
+            // Render the FPS, Gen No, and Time Left in every frame
             p.fill(255);  // White color for the text
             p.textSize(18);  // Font size
-            p.text(`FPS: ${p.round(p.frameRate())}`, 10, 20);  // Displaying the frame rate at the top-left corner
+            p.text(`FPS: ${displayedFPS}`, 10, 20);
+            p.text(`Generation: ${genCount}`, 10, 50);
+            p.text(`Time Left: ${displayedTimeLeft.toFixed(0)} seconds`, 10, 80);
+            p.text(`Top Score: ${topScoreEver}`, 10, 110);
 
             // Render NN for leading agent
             leadingAgent.renderNN(p, canvasWidth - 500, (canvasHeight / 2) - 100);
@@ -136,6 +159,9 @@ function initializeSketch(width, height, groundYPosition, gravityStrength, frict
     renderedAgents = renderedAgentsNo;
     simulationSpeed = simSpeed;
     topPerformerNo = topPerformerNumber;
+    frameCountSinceLastFPS = 0;
+    lastFPSCalculationTime = 0;
+    tickCount = 0;
 
     // If there are existing agents with models, dispose of them
     for (let agent of agents) {
@@ -149,14 +175,24 @@ function initializeSketch(width, height, groundYPosition, gravityStrength, frict
         p5Instance.remove();
         p5Instance = null;
     }
-    console.log('Number of tensors at start of sim:', tf.memory().numTensors, 'Tensor Mem at start of sim', tf.memory().numBytes);
     // Create a new p5 instance and assign it to the global reference
     p5Instance = new p5(sketch, 'canvas-container');
 }
+
+function calculateFPS(p) {
+    frameCountSinceLastFPS++;
+    let currentTime = p.millis();
+    if (currentTime - lastFPSCalculationTime > UIUpdateInterval) {
+        displayedFPS = frameCountSinceLastFPS / (UIUpdateInterval / 1000); // Convert to frames per second
+        frameCountSinceLastFPS = 0;
+        lastFPSCalculationTime = currentTime;
+    }
+}
+
 //this agent will do for now, but I intend to replace with with a dynamic body plan that can 'evolve' over time.
 //I think a JSON file defining a series of body and limb shapes, possibly with limbs connected to limbs etc
 //Starting from a random config, this would not work, as there would be little chance of initial fitness, but starting from a simple body plan and exolving complexity based on randomness and fitness might work.
-function Agent(numLimbs) {
+function Agent(numLimbs, existingBrain = null) {
     this.numLimbs = numLimbs;
     let color1 = Math.floor(Math.random() * 256);
     let color2 = Math.floor(Math.random() * 256);
@@ -239,11 +275,18 @@ function Agent(numLimbs) {
     World.add(engine.world, [this.centerBody, ...this.limbs, ...this.muscles, ...this.joints]);
 
     // Give the agent a brain!
-    this.nnConfig = nnConfig || new NeuralNetworkConfig(numLimbs); 
-    this.brain = createNeuralNetwork(this.nnConfig);
+    this.nnConfig = nnConfig || new NeuralNetworkConfig(numLimbs);
+    if (existingBrain) {
+        this.brain = existingBrain;
+    } else {
+        this.brain = createNeuralNetwork(this.nnConfig);
+    }
 
     this.getScore = function () {
         this.Score = Math.floor(this.centerBody.position.x / 10);
+        if (this.Score > topScoreEver) {
+            topScoreEver = this.Score;
+        }
         return this.Score;  // 1 point for every 10px
     };
 
@@ -288,9 +331,16 @@ function Agent(numLimbs) {
 function initializeAgents(agentProperties) {
     popSize = agentProperties.numAgents;
     limbsPerAgent = agentProperties.numLimbs;
+    genCount = 1;
     delay = delay || 50;
     for (let agent of agents) {
-        World.remove(engine.world, agent.centerBody);
+        World.remove(engine.world, [agent.centerBody, ...agent.limbs]);
+        for (let muscle of agent.muscles) {
+            World.remove(engine.world, muscle);
+        }
+        for (let joint of agent.joints) {
+            World.remove(engine.world, joint);
+        }
     }
     agents = [];  // Reset the agents array
     for (let i = 0; i < popSize; i++) {
@@ -313,6 +363,18 @@ function getLeadingAgent() {
 function endSimulation(p) {
     shouldUpdatePhysics = false;
     p.noLoop();
+
+    // Remove only the agents' bodies and constraints from the world
+    for (let agent of agents) {
+        World.remove(engine.world, [agent.centerBody, ...agent.limbs]);
+        for (let muscle of agent.muscles) {
+            World.remove(engine.world, muscle);
+        }
+        for (let joint of agent.joints) {
+            World.remove(engine.world, joint);
+        }
+    }
+
     nextGeneration(p);
 }
 
@@ -329,8 +391,6 @@ function setupMatterEngine() {
         }
     });
     World.add(engine.world, [ground]);
-    //Engine.run(engine);
-    //engine.enabled = false;
 }
 
 /*            Neural Network Functions                     */
@@ -344,7 +404,6 @@ function NeuralNetworkConfig(numLimbs) {
 
 function createNeuralNetwork(config) {
     const model = tf.sequential();
-    // console.log(model instanceof tf.Sequential);  // Should log "true"
 
     // Input layer
     model.add(tf.layers.dense({ units: config.hiddenLayers[0].nodes, activation: config.hiddenLayers[0].activation, inputShape: [config.inputNodes] }));
@@ -366,29 +425,31 @@ async function nextGeneration(p) {
     let topPerformersCount = Math.round(topPerformerNo * popSize);
     topPerformersCount = Math.max(topPerformersCount, 2); // Ensure at least 2 top performers are selected
 
+    // Create new agents for the top performers, but assign them the brains of the top performers
     for (let i = 0; i < topPerformersCount; i++) {
-        newAgents.push(agents[i]);
+        let newAgent = new Agent(agents[i].numLimbs, agents[i].brain);
+        newAgents.push(newAgent);
     }
-    console.log('Number of tensors before generating offspring:', tf.memory().numTensors, 'Tensor Mem before generating offspring', tf.memory().numBytes);
+
     // Generate offspring
     while (newAgents.length < popSize) {
         let parent1 = selectAgent(agents);
         let parent2 = selectAgent(agents);
         let childBrain = await crossover(parent1, parent2);
-        console.log('Number of tensors after crossover and clone:', tf.memory().numTensors, 'Tensor Mem after crossover and clone', tf.memory().numBytes);
         childBrain = mutate(childBrain, this.mutationRate);
-        console.log('Number of tensors after mutate:', tf.memory().numTensors, 'Tensor Mem after mutate:', tf.memory().numBytes);
-        // console.log('Child brain after crossover: ' + childBrain);
         let childAgent = new Agent(limbsPerAgent);
-        console.log('Number of tensors after creating new child:', tf.memory().numTensors, 'Tensor Mem after creating new child:', tf.memory().numBytes);
+        childAgent.brain.dispose();
         childAgent.brain = childBrain;
-        // console.log('Child brain after mutations: ' + childAgent.brain);
         newAgents.push(childAgent);
     }
 
+    // Get a list of brains in the new generation
+    let newBrains = newAgents.map(agent => agent.brain);
+
     // Dispose old agents and set the new ones
     agents.forEach(agent => {
-        if (!newAgents.includes(agent) && agent.brain) {
+        // Dispose the brain only if it's not being reused in the new generation
+        if (!newBrains.includes(agent.brain) && agent.brain) {
             agent.brain.dispose();
         }
     });
@@ -399,9 +460,9 @@ async function nextGeneration(p) {
     // Reset simulation
     shouldUpdatePhysics = true;
     tickCount = 0;
+    genCount++;
     p.loop();
 }
-
 
 function selectAgent(agents) {
     let totalFitness = agents.reduce((sum, agent) => sum + agent.getScore(), 0);
@@ -451,7 +512,6 @@ async function crossover(agent1, agent2) {
 }
 
 function mutate(childBrain, mutationRate) {
-    console.log('Number of tensors 1:', tf.memory().numTensors);
     tf.tidy(() => {
         function mutateValues(values) {
             for (let i = 0; i < values.length; i++) {
@@ -461,22 +521,17 @@ function mutate(childBrain, mutationRate) {
                 }
             }
         }
-        console.log('Number of tensors 2:', tf.memory().numTensors);
         let originalWeights = childBrain.getWeights();
-        console.log('Number of tensors 3:', tf.memory().numTensors);
         let mutatedWeights = originalWeights.map(w => {
             let values = w.arraySync();
             mutateValues(values);
             return tf.tensor(values);
         });
-        console.log('Number of tensors 4:', tf.memory().numTensors);
+
         childBrain.setWeights(mutatedWeights);
-        console.log('Number of tensors 5:', tf.memory().numTensors);
-        // Now that mutatedWeights have been set to the agent's brain, we can dispose of them
-        originalWeights.forEach(w => w.dispose());
+        // Just dispose of mutatedWeights since originalWeights tensors have not been cloned or changed
         mutatedWeights.forEach(w => w.dispose());
     });
-    console.log('Number of tensors 6:', tf.memory().numTensors);
     return childBrain; // Return the mutated childBrain
 }
 
@@ -493,9 +548,6 @@ async function cloneModel(model, config) {
 
     // Set the cloned weights to the clonedModel
     clonedModel.setWeights(weightCopies);
-
-    // Print out the weights of the clonedModel for verification
-    console.log(clonedModel.getWeights());
 
     // Dispose of the cloned weights to free up memory
     oldWeights.forEach(tensor => tensor.dispose());
@@ -614,4 +666,34 @@ Agent.prototype.updateMuscles = function () {
 
 Agent.prototype.renderNN = function (p, offsetX, offsetY) {
     renderNeuralNetwork(p, this.nnConfig, this, offsetX, offsetY);
+};
+
+Agent.prototype.reset = function () {
+    // Reset center body
+    Body.setPosition(this.centerBody, { x: 100, y: 300 });
+    Body.setVelocity(this.centerBody, { x: 0, y: 0 });
+    Body.setAngularVelocity(this.centerBody, 0);
+    this.Score = 0;
+
+    // Reset limbs
+    let angleIncrement = 2 * Math.PI / this.numLimbs;
+    let limbWidth = 10;
+    let bodyDiameter = this.centerBody.bounds.max.x - this.centerBody.bounds.min.x;
+    let bodyRadius = bodyDiameter / 2;
+    let distanceFromCenter = bodyRadius + 40 / 2; // 40 is the limb length
+
+    for (let i = 0; i < this.numLimbs; i++) {
+        let limbX = this.centerBody.position.x + distanceFromCenter * Math.cos(i * angleIncrement);
+        let limbY = this.centerBody.position.y + distanceFromCenter * Math.sin(i * angleIncrement);
+
+        Body.setPosition(this.limbs[i], { x: limbX, y: limbY });
+        Body.setAngle(this.limbs[i], i * angleIncrement + Math.PI / 2);
+        Body.setVelocity(this.limbs[i], { x: 0, y: 0 });
+        Body.setAngularVelocity(this.limbs[i], 0);
+    }
+
+    // Reset muscles to original lengths
+    for (let muscle of this.muscles) {
+        muscle.length = muscle.originalLength;
+    }
 };
